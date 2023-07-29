7bf0d6e0f1f84ccab020717e317e87b2bb73cb23
yu-heejin <jkgt0521@naver.com>
2023-07-30 01:14:01 +0900

    feat: String 클래스에 대한 학습 테스트 작성하기 1,2를 ,로 split 했을 때 1과 2로 잘 분리되는지 확인한다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,12 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1() {
+        // 배열로 반환하는 값의 경우 assertj의 contains(), containsExactly()를 활용해 반환 값이 맞는지 검증한다.
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+    }
 }
