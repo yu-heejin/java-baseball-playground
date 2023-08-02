c068b41b9db031f0117f17920f6f89772ff8982f
yu-heejin <jkgt0521@naver.com>
2023-08-02 22:39:00 +0900

    feat: String 클래스에 대한 학습 테스트 작성하기 문자열 길이 범위를 초과했을 때 발생할 수 있는 예외에 관해 테스트한다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -31,5 +32,26 @@ public class StringTest {
         // substring 메소드를 활용한다.
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 수행 시 특정 위치의 문자를 가져올 때 위치 값을 벗어나는 경우 예외 테스트")  // 테스트 메소드의 의도를 드러낸다.
+    void case3WithThrownBy() {
+        // 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 Exception이 발생한다.
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(3);
+        })
+        .isInstanceOf(IndexOutOfBoundsException.class)
+        .hasMessageContaining("String index out of range: 3");
+    }
+
+    @Test
+    @DisplayName("charAt() 수행 시 특정 위치의 문자를 가져올 때 위치 값을 벗어나는 경우 예외 테스트")
+    void case3WithExceptionOfType() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+        .isThrownBy(() -> {
+            char result = "abc".charAt(3);
+        })
+        .withMessageMatching("String index out of range: \\d+");
     }
 }
