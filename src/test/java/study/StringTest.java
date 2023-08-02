b4007db373a1b4626757aa800835ef34f8daad92
yu-heejin <jkgt0521@naver.com>
2023-08-02 22:06:53 +0900

    feat: String 클래스에 대한 학습 테스트 작성하기 contains()와 containsExcatly()의 차이점을 비교한다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -12,10 +12,17 @@ public class StringTest {
     }
 
     @Test
-    void split1() {
+    void case1WithContains() {
         // 배열로 반환하는 값의 경우 assertj의 contains(), containsExactly()를 활용해 반환 값이 맞는지 검증한다.
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
         assertThat(result).contains("2");
+    }
+
+    @Test
+    void case1WithContainsExactly() {
+        // 원소가 정확히 일치해야 한다.
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
     }
 }
