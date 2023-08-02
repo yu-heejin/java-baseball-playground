a765427a1ec165d9edc923883abfef309cdda39a
yu-heejin <jkgt0521@naver.com>
2023-08-02 22:57:45 +0900

    feat: Set Collection에 대한 학습 테스트 작성하기 numbers의 size()를 테스트한다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,4 +1,29 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void case1() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
 }
