fc483b3b1f3863968eb02ed8d87e31b9fd5854cc
yu-heejin <jkgt0521@naver.com>
2023-08-05 19:05:27 +0900

    feat: Set Collections에 대한 학습 테스트 작성하기 @ParameterizedTest 어노테이션을 사용하여 중복 코드를 없앤다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,18 @@ public class SetTest {
     void case1() {
         int result = numbers.size();
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void case2() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void case2WithParameterizedTest(int num) {
+        assertThat(numbers.contains(num)).isTrue();
     }
 }
